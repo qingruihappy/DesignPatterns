e06fab6637e0f356d9fa94553d82b12c57a34eac
qingruihappy <32031673+qingruihappy@users.noreply.github.com>
2018-10-02 11:09:13 +0800

    设计模式-----工厂模式

--- src/com/DesignPatterns/ad/factory2/Test.java
+++ src/com/DesignPatterns/ad/factory2/Test.java
@@ -20,7 +20,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		PizzaStore nyStore = new NYPizzaStore();
-		PizzaStore chicagoStore = new NYPizzaStore();
+		PizzaStore chicagoStore = new ChicagoPizzaStore();
 
 		Pizza pizza = nyStore.orderPizza("cheese");
 		System.out.println("Ethan ordered a " + pizza.getName() + "\n");
