cac840e996187055d05f00f725b7877df4b77608
qingruihappy <32031673+qingruihappy@users.noreply.github.com>
2018-10-02 16:47:05 +0800

    设计模式-----模式

--- src/com/DesignPatterns/ad/factory1/Test.java
+++ src/com/DesignPatterns/ad/factory1/Test.java
@@ -30,8 +30,6 @@ public class Test {
 		Pizza pizza = store.orderPizza("cheese");
 		System.out.println("We ordered a " + pizza.getName() + "\n");
 		System.out.println(pizza);
-		pizza = store.orderPizza("veggie");
-		System.out.println("We ordered a " + pizza.getName() + "\n");
-		System.out.println(pizza);
+
 	}
 }
