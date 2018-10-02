57efbb7b75e5475e3d996fa02816b34d1e534542
qingruihappy <32031673+qingruihappy@users.noreply.github.com>
2018-10-02 15:16:28 +0800

    设计模式-----工厂模式

--- src/com/DesignPatterns/ad/factory1/PizzaStore.java
+++ src/com/DesignPatterns/ad/factory1/PizzaStore.java
@@ -9,7 +9,6 @@ public class PizzaStore {
  
 	public Pizza orderPizza(String type) {
 		Pizza pizza;
-		fff(type);
 		pizza = factory.createPizza(type);
  
 		pizza.prepare();
@@ -18,19 +17,6 @@ public class PizzaStore {
 		pizza.box();
 
 		return pizza;
-	}
-
-	private void fff(String type) {
-		Pizza pizza;
-		if (type.equals("cheese")) {
-			pizza = new CheesePizza();
-		} else if (type.equals("pepperoni")) {
-			pizza = new PepperoniPizza();
-		} else if (type.equals("clam")) {
-			pizza = new ClamPizza();
-		} else if (type.equals("veggie")) {
-			pizza = new VeggiePizza();
-		}
 	}
 
 }
